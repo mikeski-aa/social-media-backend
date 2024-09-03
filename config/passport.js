b4158475c412780970159e86b85727e9c37d2d6b
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-09-03 14:21:47 +0200

    Written passport and db config

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const LocalStrategy = require("passport-local").Strategy;
 const passport = require("passport");
-const { PrismaClient } = require("@prisma/client");
+const { prisma } = require("../config/db");
 
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
@@ -9,3 +9,61 @@ const customFields = {
   usernameField: "email",
   passwordField: "password",
 };
+
+const verifyCallback = (email, password, done) => {
+  prisma.user
+    .findFirst({
+      where: {
+        email: email,
+      },
+    })
+    .then((user) => {
+      if (!user) {
+        console.log("USER NOT FOUND");
+        // user not present in DB
+        return done(null, false);
+      }
+
+      const isValid = validatePassword(password, user.hash);
+      if (isValid) {
+        console.log("validation OK");
+        return done(null, user);
+      } else {
+        console.log("wrong pass");
+        return done(null, false);
+      }
+    })
+    .catch((error) => {
+      done(error);
+    });
+};
+
+// new strat requires verify callback
+const strategy = new LocalStrategy(customFields, verifyCallback);
+passport.use(strategy);
+
+/// JWT strategy part
+const verifyJWTCallback = (jwt_payload, done) => {
+  prisma.user
+    .findFirst({
+      where: {
+        email: jwt_payload.email,
+      },
+    })
+    .then((user) => {
+      return done(null, user);
+    })
+    .catch((error) => {
+      done(error, false, { message: "Token missing" });
+    });
+};
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "secret",
+    },
+    verifyJWTCallback
+  )
+);
