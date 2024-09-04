7c476a95919759a6447af2fb5be514f7f6065570
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-09-04 11:51:08 +0200

    Working on register user backend

---
+++ controllers/apiController.js
@@ -0,0 +1,4 @@
+const asyncHandler = require("express-async-handler");
+const { genPassword } = require("../lib/passportUtils");
+
+exports.postRegister = asyncHandler(async (req, res, next) => {});
--- middleware/validateInput.js
+++ middleware/validateInput.js
@@ -1,5 +1,21 @@
-const { body } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
-function validateInput(req, res, next) {
-  next();
-}
+exports.registerValidation = [
+  body("username").exists().length({ min: 1, max: 15 }).trim().escape(),
+  body("email").exists().length({ min: 1 }).isEmail().trim().escape(),
+  body("password").exists().length({ min: 1 }).trim().escape(),
+  body("confirmPassword").exists().length({ min: 1 }).trim().escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: "Input validation failed" });
+    }
+
+    if (req.body.password != req.body.confirmPassword) {
+      return res.status(400).json({ message: "Password mismatch" });
+    }
+
+    return next();
+  },
+];
